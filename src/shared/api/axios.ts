81e8e33d8d482a301c93a742d0fbb88b00e09be3
codefug <robot9917@naver.com>
2024-07-19 23:34:22 +0900

    📦 chore : RTR 로직 삭제..

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../constants/constants";
-import { refreshTokenRotation } from "../util/RTR";
 
 const instance = axios.create({ baseURL: BASE_URL });
 
-const authInstance = axios.create({ baseURL: BASE_URL });
-
-const { setAuthHeader } = refreshTokenRotation();
-
-authInstance.interceptors.request.use(setAuthHeader);
-
-export { authInstance, instance };
+export { instance };
