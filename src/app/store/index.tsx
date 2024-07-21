c2da375634b9f81fb1c286a08aac3bc89636eb55
codefug <86544979+codefug@users.noreply.github.com>
2024-07-21 10:45:33 +0900

    ✨ feat : zustand 로그인 로직에 refresh token 저장 기능 추가

--- src/app/store/index.tsx
+++ src/app/store/index.tsx
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import { create, StateCreator } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -7,13 +8,16 @@ type User = {
 
 type UserState = {
   user: User | null;
-  login: (accessToken: string) => void;
+  login: (accessToken: string, refreshToken: string) => void;
   logout: () => void;
 };
 
 export const useStoreSlice: StateCreator<UserState> = (set) => ({
   user: null,
-  login: (accessToken: string) => set(() => ({ user: { accessToken } })),
+  login: (accessToken: string, refreshToken: string) => {
+    Cookies.set("refreshToken", refreshToken, { expires: 1 });
+    return set(() => ({ user: { accessToken } }));
+  },
   logout: () => set(() => ({ user: null })),
 });
 
